bd84b2edd035f83e5620e9af95f47c361a09da28
CrunchyBreadcrumbs <144739569+CrunchyBreadcrumbs@users.noreply.github.com>
2024-08-02 12:18:16 +0200

    Create html, css and JS files and write code

--- myscript.js
+++ myscript.js
@@ -19,52 +19,36 @@ function createGrid(number){
     divArray = [];
     containerDiv.replaceChildren();
     for (let i = 0; i < number; i++){
-        const divGrid = containerDiv.appendChild(document.createElement("div"));
-        divGrid.classList.add("grid", "row");
+        const rowDiv = containerDiv.appendChild(document.createElement("div"));
+        rowDiv.classList.add("row");
+        let width = 100 / number;
 
         for(let k = 0; k < number; k++){
-        const parentGrid = document.querySelectorAll(".row");
-        let last = parentGrid[parentGrid.length-1];
-        const rowGrid = last.appendChild(document.createElement("div"));
-        rowGrid.classList.add("grid");
-        rowGrid.addEventListener("mouseover", () =>{
-            rowGrid.classList.add("hovered");
-        });
+            const parentGrid = document.querySelectorAll(".row");
+            let last = parentGrid[parentGrid.length-1];
+            const square = last.appendChild(document.createElement("div"));
+            square.classList.add("grid");
+
+            square.style.flexBasis = `${width}%`;
+            square.style.height = `${width}%`;
+
+            square.addEventListener("mouseenter", () =>{
+                square.classList.add("hovered");
+
+                let currentOpacity = square.style.opacity;
+
+                if(currentOpacity <= 0.9){
+                    square.style.opacity = +square.style.opacity + 0.1;
+                }
+
+                let colors = ["#73d1c9", "#7FFFD4", "#71abd1", "#738ed1"];
+                let random_color = colors[Math.floor(Math.random()*colors.length)];
+                square.style.backgroundColor = random_color;
+  
+            });
         }
+        divArray.push(rowDiv);
 
-        
-        divArray.push(divGrid);
-
-        divArray[i].addEventListener("mouseover", () =>{
-            divArray[i].classList.add("hovered");
-        });
-
-        
-    };
-
-    // for (let k = 0; k < number; k++){
-    //     const rowGrid = containerDiv.appendChild(document.createElement("div"));
-    //     divGrid.classList.add("grid", "row");
-
-
-        
-    //     divArray.push(divGrid);
-
-    //     divArray[i].addEventListener("mouseover", () =>{
-    //         divArray[i].classList.add("hovered");
-    // }
-
-    
+        divArray[i].style.flexBasis = `${width}%`; 
+    };    
 }
-
-
-
-
-// let width = 100 / number;
-
-//         document.querySelector(".row").style.flexBasis = `${width}%`;
-
-// for(let k = 0; i < number; k++){
-//     const rowGrid = divGrid.appendChild(document.createElement("div"));
-//     rowGrid.classList.add("grid");
-// }
